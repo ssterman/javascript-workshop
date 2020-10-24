c5367a4105a692780d798def43316b4f7b95fee0
Sarah Sterman <ssterman@berkeley.edu>
2020-10-24 10:33:09 -0400

    more buggy exercises

---
+++ python_examples/nasa_bug_4.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as pyplot
+  
+# Opening JSON file 
+with open('asteroids.json') as json_file: 
+    data = json.load(json_file) 
+    # print("Type:", type(data))  # Print the type of data variable 
+    # print(data['near_earth_objects']['2020-10-25'][0]) # Print the data format to see what it looks like
+
+    # extract the data we want  
+    miss_distances = []
+    diameters = []
+    for i in range(0, 20):
+        asteroid = data['near_earth_objects']['2020-10-25'][i]
+        miss_distance = asteroid['close_approach_data'][0]['miss_distance']['kilometers']
+        miss_distances.append(miss_distance)
+        diameter = asteroid['estimated_diameter']['kilometers']['estimated_diameter_max']
+        diameters.append(diameter)
+
+
+    # Use Matplotlib to visualize the data
+    plot = pyplot.subplot(111)
+    scatter = plot.scatter(miss_distances, diameters)
+            # , s=None, c=None, marker=None, cmap=None, norm=None, vmin=None, vmax=None, alpha=None, linewidths=None, verts=<deprecated parameter>, edgecolors=None, *, plotnonfinite=False, data=None, **kwargs)[
+    
+    plot.set_ylabel('diameter (km)')
+    plot.set_xlabel('earth miss distance (km)')
+    pyplot.show()
